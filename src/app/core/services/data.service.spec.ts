fcc110125ef92789e97214a6f433e9b3c863f1ee
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2023-03-31 21:04:13 +0200

    Integrate with SonarQube

--- src/app/core/services/data.service.spec.ts
+++ src/app/core/services/data.service.spec.ts
@@ -1,16 +1,34 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DataService } from './data.service';
+import { HttpErrorHandlerService } from './http-error-handler.service';
 
 describe('Service: Data', () => {
+
+  let service: DataService;
+  let httpTestingController: HttpTestingController;
+  let httpErrorHandlerServiceSpy: jasmine.SpyObj<HttpErrorHandlerService>;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataService]
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        DataService,
+        {
+          provide: HttpErrorHandlerService,
+          useValue: jasmine.createSpyObj('HttpErrorHandlerService', ['createHandleError'])
+        }
+      ]
     });
+
+    service = TestBed.inject(DataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    httpErrorHandlerServiceSpy = TestBed.inject(HttpErrorHandlerService) as jasmine.SpyObj<HttpErrorHandlerService>;
   });
 
-  it('should ...', inject([DataService], (service: DataService) => {
+  it('should ...', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
--- src/app/core/services/http-error-handler.service.spec.ts
+++ src/app/core/services/http-error-handler.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpErrorHandlerService } from './http-error-handler.service';
+import { MessageService } from 'primeng/api';
 
 describe('Service: HttpErrorHandler', () => {
 
   let service: HttpErrorHandlerService;
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpErrorHandlerService]
+      providers: [
+        HttpErrorHandlerService,
+        {
+          provide: MessageService,
+          useValue: jasmine.createSpyObj('MessageService', ['add'])
+        }
+      ]
     });
 
     service = TestBed.inject(HttpErrorHandlerService);
+    messageServiceSpy = TestBed.inject(MessageService) as jasmine.SpyObj<MessageService>;
   });
 
   it('should init', () => {
